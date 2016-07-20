caa59ad643ca8d2a07d8dc8bc6fa608d0fefbfa2
RichardRoda <RichardRoda@users.noreply.github.com>
2016-07-19 22:50:12 -0400

    Add JSDoc comments to the secureStorage facade.

--- common/SecureLocalStorage.js
+++ common/SecureLocalStorage.js
@@ -17,10 +17,24 @@ function secureStorage(encKey,storage) {
 
     var hmac = new sjcl.misc.hmac(sjcl.codec.hex.toBits(encKey), sjcl.hash.sha256);
 
+    /**
+     * Encrypt the value.
+     * @param {Object} value Object to encrypt.
+     * @returns {sjcl.json.encrypt} JSON representation of the encrypted object.
+     */
     this.encrypt = function(value) {
         return sjcl.encrypt(encKey, value);
     }
 
+    /**
+     * Set the specified key to the specified value.  Also stores a
+     * HMAC-SHA256 hash of the value which is used to detect possible
+     * tampering.
+     * 
+     * @param {type} key Key to associate with the value.
+     * @param {type} value Value to store.
+     * @returns {undefined}
+     */
     this.setStorage = function (key,value) {
         var encryptedData = this.encrypt(value);
         var hashedKey = this.getHMAC(key);
@@ -29,15 +43,32 @@ function secureStorage(encKey,storage) {
         storage[hashedKey] = encryptedData;
         storage[hashAuthCodeKey] = hashAuthCode;
     }
-    
+
+    /**
+     * Key with a suffix that is used as the key for the HMAC-SHA code to
+     * validate that a value is untampered with.
+     * @param {type} key Key used to store the value.
+     * @returns {String} Key that will store the HMAC-SHA256 code.
+     */
     this.getHashAuthCodeKey = function (key) {
         return this.getHMAC(key + 'com.hpe.demo.storage.protection');
     }
-    
+
+    /**
+     * Return the decrypted value.
+     * @param {type} cypherText From encrypt function.
+     * @returns {sjcl.json.decrypt} Decrypted value.
+     */
     this.decrypt = function (cypherText) {
         return sjcl.decrypt(encKey, cypherText);
     }
 
+    /**
+     * Get, decrypt, and verify the value in storage.
+     * @param {type} key Key that this value was set under.
+     * @returns {sjcl.json.decrypt} Decrypted and validated values.
+     * @throws {String} If HMAC mismatches (value was altered after being stored).
+     */
     this.getStorage = function (key) {
         var cypherText = storage[this.getHMAC(key)];
         var value = this.decrypt(cypherText);
@@ -49,7 +80,12 @@ function secureStorage(encKey,storage) {
         }
         return value;
     }
- 
+
+    /**
+     * Get HMAC code for the specified key or value.
+     * @param {type} key Key or value to obtain code.
+     * @returns {String} Base64 encoded string of the computed code.
+     */
     this.getHMAC = function (key) {
         bitArray = hmac.encrypt(key);
         return sjcl.codec.base64.fromBits(bitArray);  
